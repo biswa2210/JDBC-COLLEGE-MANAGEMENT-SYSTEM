476a8533c85a542eb90d02575d26f2c6bdc325c8
Biswarup Bhattacharjee <74323889+biswa2210@users.noreply.github.com>
2021-05-16 18:33:29 +0530

    Update ConnectionOpratios.java

--- src/com/student/attedence/ConnectionOpratios.java
+++ src/com/student/attedence/ConnectionOpratios.java
@@ -4,7 +4,11 @@ import java.sql.*;
 import java.util.Arrays;
 
 import com.student.attedence.*;
-
+/*
+CREATED BY BISWARUP BHATTACHARJEE
+EMAIL    : bbiswa471@gmail.com
+PHONE NO : 6290272740
+*/
 
 
 public class ConnectionOpratios {
